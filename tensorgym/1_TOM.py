21ca16614921217abc98d42a113aa027c69d1c3c
yuro-py <zerone.taken@gmail.com>
2026-05-17 06:30:17 +0530

    TOM 1 DONE

--- tensorgym/1_TOM.py
+++ tensorgym/1_TOM.py
@@ -3,9 +3,9 @@ import torch
 
 def add_tensors(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
     result = torch.add(x, y)
-    return result
+    return result.tolist()
 
 a = torch.tensor([12,34,56], dtype=torch.int32)
 b = torch.tensor([78,90,12], dtype=torch.int32)
-c = add_tensors(a, b).tolist()
+c = add_tensors(a, b)
 print(c)
